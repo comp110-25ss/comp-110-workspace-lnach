347d688c010315dd734bb45f0064f3554e91210c
Luke Nachnani <lukenachnani05@gmail.com>
2025-07-02 14:07:59 -0400

    ex00 and cq00 finished

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""Practice writing functions"""
+
+__author__ = "730825624"
+
+
+def mimic(message: str) -> str:
+    """This function will take your input and return it back to you"""
+    return message
+
+
+def main() -> None:
+    """Pulls tg all functions and implements the high-level logic of the program"""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Lessons/functions.py
@@ -0,0 +1,9 @@
+"""Practice writing and calling functions"""
+
+
+def sum(num1: int, num2: int) -> int:
+    """Return the sum of the 2 numbers"""
+    return num1 + num2
+
+
+print(sum(num1=11, num2=3))
